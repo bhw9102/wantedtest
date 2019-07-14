985ab492a870ad19939f5fbff1cb21ce079bd8c4
bhw9102 <bhw9102@gmail.com>
2019-07-14 18:56:58 +0900

    작업 내용에 주석을 남기다.

--- wantedtest/models.py
+++ wantedtest/models.py
@@ -3,6 +3,11 @@ from sqlalchemy.orm import relationship, backref
 
 
 class Language(db.Model):
+    """
+    번역될 언어 목록
+    name : 언어명
+    code : 언어의 코드명
+    """
     __tablename__ = 'language'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20), nullable=False)
@@ -11,6 +16,10 @@ class Language(db.Model):
 
 
 class NameBase(db.Model):
+    """
+    번역의 대상이 되는 Company, Tag 의 상위모델
+    name : 업무 효율을 위한 대표성을 띄는 이름, 실제 각 언어별 대응은 따로한다.
+    """
     __tablename__ = 'name_base'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(40), nullable=False)
@@ -20,7 +29,6 @@ class NameBase(db.Model):
 class Company(NameBase):
     """
     회사 데이터
-    name : 각 회사를 대표할 수 있는 회사명, 각 언어 대응은 따로 한다.
     """
     __tablename__ = 'company'
     id = db.Column(db.Integer, db.ForeignKey('name_base.id'), primary_key=True)
@@ -28,12 +36,19 @@ class Company(NameBase):
 
 
 class Tag(NameBase):
+    """
+    태그 데이터
+    """
     __tablename__ = 'tag'
     id = db.Column(db.Integer, db.ForeignKey('name_base.id'), primary_key=True)
     company_list = relationship('Company', secondary='attaching')
 
 
 class Attaching(db.Model):
+    """
+    회사별, 태그 등록 데이터
+    Many to Many relationship
+    """
     __tablename__ = 'attaching'
     id = db.Column(db.Integer, primary_key=True)
     company_id = db.Column(db.Integer, db.ForeignKey('company.id'))
@@ -43,6 +58,10 @@ class Attaching(db.Model):
 
 
 class Localization(db.Model):
+    """
+    각 번역 대상들의 언어별 번역 데이터
+    Many to Many relationship
+    """
     __tablename__ = 'localization'
     id = db.Column(db.Integer, primary_key=True)
     name_base_id = db.Column(db.Integer, db.ForeignKey('name_base.id'))
