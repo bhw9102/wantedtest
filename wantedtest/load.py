b90c86113b24da47f1db9fbbe634ab71ec104a10
bhw9102 <bhw9102@gmail.com>
2019-07-14 13:53:32 +0900

    회사, 태그, 지역화 대응 모델을 생성하고, db와 연계하여 작동하는지 확인한다.

--- wantedtest/load.py
+++ wantedtest/load.py
@@ -1,13 +1,32 @@
 from os import path
 from wantedtest import db
-from wantedtest.models import Company
+from wantedtest.models import Company, Tag, Attaching, Language
 
 
 def create_company():
-    company = Company(id=1, name='test')
+    company = Company(id=1, name='company_1')
+    tag = Tag(id=1, name='tag_1')
+    company.tag_list.append(tag)
     db.session.add(company)
     db.session.commit()
 
 
+def create_tag():
+    tag = Tag(id=2, name='tag_2')
+    db.session.add(tag)
+    db.session.commit()
+
+
+def create_language():
+    language = Language(id=1, name='Korean', code='ko')
+    db.session.add(language)
+    db.session.commit()
+
+
+db.reflect()
+db.drop_all()
 db.create_all()
 create_company()
+create_tag()
+create_language()\
+
--- wantedtest/models.py
+++ wantedtest/models.py
@@ -1,11 +1,39 @@
 from wantedtest import db
+from sqlalchemy.orm import relationship, backref
 
 
 class Company(db.Model):
+    """
+    회사 데이터
+    name : 각 회사를 대표할 수 있는 회사명, 각 언어 대응은 따로 한다.
+    """
     __tablename__ = 'company'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20), nullable=False)
+    tag_list = relationship('Tag', secondary='attaching')
 
+
+class Tag(db.Model):
+    __tablename__ = 'tag'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), nullable=False)
+    company_list = relationship('Company', secondary='attaching')
+
+
+class Attaching(db.Model):
+    __tablename__ = 'attaching'
+    id = db.Column(db.Integer, primary_key=True)
+    company_id = db.Column(db.Integer, db.ForeignKey('company.id'))
+    tag_id = db.Column(db.Integer, db.ForeignKey('tag.id'))
+    company = relationship(Company, backref=backref('attaching', cascade='all, delete-orphan'))
+    tag = relationship(Tag, backref=backref('attaching', cascade='all, delete-orphan'))
+
+
+class Language(db.Model):
+    __tablename__ = 'language'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), nullable=False)
+    code = db.Column(db.String(5), nullable=False)
 
 
 
