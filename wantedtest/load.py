d43ea72636a5659d5428befbc206e061af8a0a22
bhw9102 <bhw9102@gmail.com>
2019-07-14 15:47:17 +0900

    언어 번역을 준비하다.

--- wantedtest/load.py
+++ wantedtest/load.py
@@ -1,6 +1,24 @@
-from os import path
+import config
+from xlrd import open_workbook
 from wantedtest import db
 from wantedtest.models import Company, Tag, Attaching, Language
+
+
+def load_keys(sheet):
+    return [sheet.cell_value(0, i) for i in range(sheet.ncols)]
+
+
+def add_rows(sheet, model):
+    for row in range(sheet.nrows - 1):
+        data = {}
+        keys = load_keys(sheet)
+        for col, key in enumerate(keys):
+            data[key] = sheet.cell_value(row + 1, col)
+            if type(data[key]) is float:
+                data[key] = int(data[key])
+        m = model(**data)
+        db.session.add(m)
+    db.session.commit()
 
 
 def create_company():
@@ -18,15 +36,19 @@ def create_tag():
 
 
 def create_language():
-    language = Language(id=1, name='Korean', code='ko')
-    db.session.add(language)
-    db.session.commit()
+    data_path = config.BASE_DIR + '/wantedtest/import/language.xlsx'
+    dir_strings = data_path.split('/')
+    dir_strings = [i for i in dir_strings]
+    import_data_path = '/'.join(dir_strings)
+    wb = open_workbook(import_data_path)
+    target_sheet = wb.sheet_by_name('language')
+    add_rows(sheet=target_sheet, model=Language)
 
 
 db.reflect()
 db.drop_all()
 db.create_all()
-create_company()
-create_tag()
-create_language()\
+create_language()
+
+
 
--- wantedtest/models.py
+++ wantedtest/models.py
@@ -2,21 +2,34 @@ from wantedtest import db
 from sqlalchemy.orm import relationship, backref
 
 
-class Company(db.Model):
+class Language(db.Model):
+    __tablename__ = 'language'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), nullable=False)
+    code = db.Column(db.String(5), nullable=False)
+    name_base_list = relationship('NameBase', secondary='localization')
+
+
+class NameBase(db.Model):
+    __tablename__ = 'name_base'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), nullable=False)
+    language = relationship('Language', secondary='localization')
+
+
+class Company(NameBase):
     """
     회사 데이터
     name : 각 회사를 대표할 수 있는 회사명, 각 언어 대응은 따로 한다.
     """
     __tablename__ = 'company'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20), nullable=False)
+    id = db.Column(db.Integer, db.ForeignKey('name_base.id'), primary_key=True)
     tag_list = relationship('Tag', secondary='attaching')
 
 
-class Tag(db.Model):
+class Tag(NameBase):
     __tablename__ = 'tag'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20), nullable=False)
+    id = db.Column(db.Integer, db.ForeignKey('name_base.id'), primary_key=True)
     company_list = relationship('Company', secondary='attaching')
 
 
@@ -29,11 +42,12 @@ class Attaching(db.Model):
     tag = relationship(Tag, backref=backref('attaching', cascade='all, delete-orphan'))
 
 
-class Language(db.Model):
-    __tablename__ = 'language'
+class Localization(db.Model):
+    __tablename__ = 'localization'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20), nullable=False)
-    code = db.Column(db.String(5), nullable=False)
-
-
+    name_base_id = db.Column(db.Integer, db.ForeignKey('name_base.id'))
+    language_id = db.Column(db.Integer, db.ForeignKey('language.id'))
+    name_base = relationship(NameBase, backref=backref('localization', cascade='all, delete-orphan'))
+    language = relationship(Language, backref=backref('localization', cascade='all, delete-orphan'))
+    value = db.Column(db.String(20), nullable=False)
 
